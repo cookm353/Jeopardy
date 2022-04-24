0d03485ed56e82371aec444dff036302bfbf0f1f
Matthew Cook <cookm353@gmail.com>
2022-04-24 07:36:03 -0400

    Two steps forward, one step back

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Jeopardy
+## General structure
+* HTML table w/ categories in the thead and questions in tbody
+
+## Category class
+* Hold the name of the category and each question
+* Does it need any methods?
+
+## Board class
+* Methods to fill each row with questions
+* thead: 6 columns
+* tbody: 5 rows of questions, 6 columns per row
+
+
+## Game class
+* Mostly event handlers methods
+* Does it need any properties?
+
+
+
+
+
+## Game board
+7. Append categories to th elements and clues to td elements
+8. Add 
+
+
+**Ask Archana how to refactor the functions for the API calls to a more generalized version that can be used for both categories and clues!**
+
+
+**Day 1**
+* 2.5 hrs
+* Created rough outline of program's logic
+* Finished w/ API calls
+* (Probably) done with category class
+* Ready to tackle the HTML elements tomorrow!
+
+* Create new game object
+* Move logic for creating categories into methods in game
+
+
+**Day 2**
+Realized that JS refuses to retrieve clues from the data structure I created, have to scrap it and start again (or at least dramatically refactor code)
+
+* Retrieve all of the categories, store id and title
+* Winnow down to 6
+* Loop thru categories
+    * Each time make API call for clues
+* Remake the 
--- jeopardy.js
+++ jeopardy.js
@@ -96,3 +96,177 @@ async function setupAndStart() {
 /** On page load, add event handler for clicking clues */
 
 // TODO
+
+class Board {
+	constructor() {
+		this.$thead = $("thead");
+		this.$tbody = $("tbody");
+		this.makeHtmlBoard();
+	}
+	/** Make rows and table cells for both head and body */
+	makeHtmlBoard() {
+		// Add cells to table head
+		for (let col = 0; col < 6; col++) {
+			const $th = $("<th></th>");
+			this.$thead.append($th);
+		}
+
+		// Make rows, adds cells to rows, and add rows to table body
+		for (let row = 0; row < 5; row ++) {
+			const $tr = $("<tr></tr>")
+			for (let col = 0; col < 6; col ++) {
+				const $td = $("<td></td>");
+				$tr.append($td);
+			}
+			this.$tbody.append($tr);
+		}
+	}
+	addCategories(categories) {
+		for (let i = 0; i < 6; i++) {
+			$("th").eq(i).text(categories[i].title);
+		}
+	}
+	addClues(categories) {
+		// Loop thru each tr
+		const $trs = $("tr");
+		console.log(categories[0].clues);
+
+		for (let i = 0; i < $trs.length; i++) {
+			const $tds = $trs.eq(i).children;
+			// Loop thru each category
+			for (let j = 0; j < categories.length; j++) {
+				const td = $tds[j];
+				const clues = categories[j].clues
+				// console.log(categories[j])
+				// const {question, answer, showing} = categories[j].clues[i];
+				
+				const clueTile = this.makeClueTile(clues);
+			}
+		}
+		for(let category of categories) {
+			const clues = category.clues
+			// console.log(typeof clues);
+			for (let [k, v] of Object.entries(clues)) {
+				console.log(`${k}: ${v}`)
+			}
+			// for (let clue of category.clues) {
+			// 	console.log(clue)
+			// }
+		}
+	}
+	makeClueTile(clues) {
+		const $div1 = $(`<div class=showing>`)
+		for (let clue of clues) {
+			console.log(clue)
+		}
+	}
+}
+
+class Category {
+	constructor(id, title) {
+		this.id = id
+		this.title = title
+		this.clues = []
+	}
+	/**
+	 * Retrieve 5 random clues from category and store in object
+	 */
+	async getClues() {
+		const cluesUrl = `http://jservice.io/api/clues?id=${this.id}`
+		const clueIndices = getRandomIndices(5);
+
+		try {
+			const clues = await axios.get(cluesUrl)
+			for (let i of clueIndices) {
+				const clue = clues.data[i];
+				this.clues.push({
+					question: clue.question,
+					answer: clue.answer,
+					showing: null
+				});
+			}
+		} catch {
+			alert("Error: Could not retrieve clues")
+		}
+	}
+
+	/**
+	 * Retrieve 100 categories from API
+	 */
+	static async getCategories() {
+		const categoriesUrl = `http://jservice.io/api/categories?count=100`;
+		try {
+			const resp = await axios.get(categoriesUrl);
+			return resp;
+		} catch {
+			alert("Error: could not retrieve categories");
+		}
+	}
+	static pickRandomCategories(categoriesResp, indices){
+		for (let i of indices) {
+			const id = categoriesResp.data[i].id;
+			const title = categoriesResp.data[i].title;
+			// console.log(id, title)
+			const category = new Category(id, title);
+			categories.push(category)
+		}
+
+		return categories;
+	}
+}
+
+class Game {
+	constructor() {
+		this.categories = [];
+	}
+}
+
+/** Generate specified number of random indices 
+ * to select random categories and clues */
+function getRandomIndices(numIndices) {
+	const indices = [];
+	while (indices.length < numIndices) {
+		const index = Math.floor(Math.random() * 100);
+		if (indices.indexOf(index) === -1) {
+			indices.push(index)
+		}
+	}
+
+	return indices;
+}
+
+/** Button to start a new game */
+$("#startGameBttn").on("click", async function(evt) {
+	// evt.preventDefault();
+	const jeopardy = new Game();
+	
+	const categoriesResp = await Category.getCategories();	
+	const categoryIndices = getRandomIndices(6);
+	jeopardy.categories =  Category.pickRandomCategories(categoriesResp,
+		categoryIndices);
+
+	for ( let category of categories ) {
+		category.getClues();
+	}
+	
+	const board = new Board();
+	board.addCategories(jeopardy.categories);
+
+	for (let category of categories) {
+		console.log(category.clues[0])
+	}
+	
+	
+	/*
+	// Randomly select 6 categories
+	const categoriesResp = await Category.getCategories();	
+	const categoryIndices = getRandomIndices();
+	jeopardy.categories = Category.pickRandomCategories(categoriesResp, categoryIndices);
+	
+	// Retrieve clues for each category and add to category object
+	for ( let category of categories ) {
+		category.getClues();
+	}
+	*/
+})
+
